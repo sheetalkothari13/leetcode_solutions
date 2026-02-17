b792c707ac3d1662ed71c4958ffc7d277c5e4d06
K Sheetal Kothari <21q91a66e1@mrce.in>
2026-02-18 01:29:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16.01%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -2,12 +2,8 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         n = len(nums)
         s = 0
-        c = 0
         for i in range(n):
             if nums[i] != val:
                 nums[i],nums[s] = nums[s],nums[i]
                 s += 1
-            else:
-                c += 1
-        # nums = nums[:-c]
-        return (n-c)
+        return s
