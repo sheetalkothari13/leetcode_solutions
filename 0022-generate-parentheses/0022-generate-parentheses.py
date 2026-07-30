f13322bda595163c4de3398b0d1088eee5221706
K Sheetal Kothari <21q91a66e1@mrce.in>
2026-07-31 00:17:56 +0530

    Time: 1 ms (41.88%), Space: 19.4 MB (72.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,23 +4,19 @@ class Solution:
         current = []
 
         def backtrack(open_used, close_used):
-
-            # Base case: we have used all parentheses
             if len(current) == 2 * n:
                 result.append("".join(current))
                 return
 
-            # Option 1: Add '(' if we still have some left
             if open_used < n:
                 current.append("(")
                 backtrack(open_used + 1, close_used)
-                current.pop()      # Backtrack
+                current.pop()   
 
-            # Option 2: Add ')' only if it won't make the string invalid
             if close_used < open_used:
                 current.append(")")
                 backtrack(open_used, close_used + 1)
-                current.pop()      # Backtrack
+                current.pop()      
 
         backtrack(0, 0)
         return result
