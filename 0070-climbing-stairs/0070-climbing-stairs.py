dc0ce23fc7a8e2690ee0d8d8239eeae7ce5d68fb
K Sheetal Kothari <21q91a66e1@mrce.in>
2026-08-08 10:37:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (52.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,17 @@
 class Solution:
-    c = {}
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        if n in self.c:
-            return self.c[n]
-        self.c[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
-        return self.c[n]
-        
+        c = {}
+
+        def solve(n):
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+
+            if n in c:
+                return c[n]
+
+            c[n] = solve(n - 1) + solve(n - 2)
+            return c[n]
+
+        return solve(n)
