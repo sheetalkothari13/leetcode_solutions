c43e99e3cce9d84ca0a764406d0ab8fc03fcd08e
K Sheetal Kothari <21q91a66e1@mrce.in>
2026-08-31 16:19:59 +0530

    Time: 154 ms (80.36%), Space: 41.7 MB (82.3%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,36 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.left = []    
+        self.right = []   
+
+    def addNum(self, num: int) -> None:
+        if not self.right or num >= self.right[0]:
+            heapq.heappush(self.right, num)
+        else:
+            heapq.heappush(self.left, -num)
+
+        if len(self.left) > len(self.right) + 1:
+            heapq.heappush(
+                self.right,
+                -heapq.heappop(self.left)
+            )
+
+        elif len(self.right) > len(self.left) + 1:
+            heapq.heappush(
+                self.left,
+                -heapq.heappop(self.right)
+            )
+
+    def findMedian(self) -> float:
+
+        if len(self.left) > len(self.right):
+            return -self.left[0]
+
+        elif len(self.right) > len(self.left):
+            return self.right[0]
+
+        else:
+            return (-self.left[0] + self.right[0]) / 2
