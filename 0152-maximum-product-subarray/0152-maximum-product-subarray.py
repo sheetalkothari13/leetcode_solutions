a0109426ce69d49ea80de32133cac126c9d10b67
K Sheetal Kothari <21q91a66e1@mrce.in>
2026-09-14 09:02:03 +0530

    Time: 3 ms (90.41%), Space: 19.8 MB (45.78%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -4,11 +4,10 @@ class Solution:
         max_prod = nums[0]
         min_prod = nums[0]
         ans = nums[0]
-
         for i in range(1,n):
-            if nums[i] < 0:
-                max_prod, min_prod = min_prod, max_prod
-            max_prod = max(nums[i], max_prod*nums[i])
-            min_prod = min(nums[i], min_prod*nums[i])
+            if nums[i]<0:
+                max_prod,min_prod = min_prod,max_prod
+            max_prod = max(nums[i], nums[i]*max_prod)
+            min_prod = min(nums[i], nums[i]*min_prod)
             ans = max(ans,max_prod)
         return ans
